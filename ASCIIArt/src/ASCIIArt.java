78a3d162e7b48e2a73bb24b8de7a4adb7bfda8c3
s-lijer <s-lijer@53S500038105.it.bsd405.org>
2019-09-20 08:08:31 -0700

    Turning in Project 1

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -1,27 +1,36 @@
-/**
- * Unit 1 Project
- * 
- * In this project you have the opportunity to create a piece of artwork using ASCII 
- * (American Standard Code for Information Interchange). Using the already created file 
- * named `ASCIIArt.java` write a program that outputs your artwork to the console. 
- * The goal of this project is to use ONLY what we have learned in Unit 1 to create 
- * anything you would like so be creative (and school appropriate). Artwork must also have a caption describing what it's depicting.  
- * 
- * For those of you who have dabbled in coding before, try to contain your code to 
- * just what we have discussed in this unit.  
- * 
- * @author Mr. Rasmussen 
- *
- */
-
-// YOU CODE HERE :) 
-
 public class ASCIIArt {
+	
 	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+		pumpkinTop();
+		pumpkinFace();
+		pumpkinBase();
+		spookyQuote();
 	}
+	
+	public static void pumpkinTop() {
+		System.out.println("              __");
+		System.out.println("              \\ \\");
+		System.out.println("       _______/  \\________");
+		System.out.println("     _/                   \\_");
+		
+	}
+	
+	public static void pumpkinFace() {
+		System.out.println("    /     __         __     \\");
+		System.out.println("   /      \\_\\       /_/      \\");
+		System.out.println("   |     __           __     |");
+		System.out.println("   |     \\ \\_________/ /     |");
+		System.out.println("   \\      \\___________/      /");
+	}
+	
+	public static void pumpkinBase() {
+		System.out.println("    \\_                     _/");
+		System.out.println("      \\___________________/");
+	}
+	
+	public static void spookyQuote() {
+		System.out.println("");
+		System.out.println("\"September, the spookiest time of the year.\" -Miles Davis");
+	}
+	
 }
